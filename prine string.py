a7a47d8f9b13f4d3ce287c3fdf8ab5693285b11e
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-14 16:51:50 +0700

    test

---
+++ prine string.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+print('INE')
+print("Hello Anirach you will be great")
+print("I'm hungry")
+print('He ask me "Who got the book?"')
+print("I'm good and \"I will domy best\"")
+print('/\\')
+print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
